4d77281db4e6e4904ed4772d5c4f0802946503a6
Tahir Ishaq <tahirishaq10@gmail.com>
2025-06-24 18:52:23 +0200

    Updated weights to reopt and enabled metric curves

--- download_weights.sh
+++ download_weights.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-base_url="https://github.com/meituan/YOLOv6/releases/download/0.4.0/yolov6WEIGHT.pt"
+#base_url="https://github.com/meituan/YOLOv6/releases/download/0.4.0/yolov6WEIGHT.pt"
+reopt_base_url="https://github.com/meituan/YOLOv6/releases/download/0.2.0/yolov6"
+
 mkdir weights
 
 for weight in s n
 do
-    echo "Downloading yolov6$weight"
-    url="${base_url/WEIGHT/"$weight"}"
-    wget -q $url -O weights/yolov6$weight.pt
-    echo "Finished downloading yolov6$weight.pt"
+    model_name="yolov6"$weight"_v2_reopt.pt"
+    echo "Downloading $model_name"
+    if [ "$weight" = "s" ]; then
+        url=$reopt_base_url$weight"_v2_reopt.pt"
+    else
+        url=$reopt_base_url$weight"_v2_repopt.pt"
+    fi
+    wget -q $url -O weights/$model_name
+    echo "Finished downloading $model_name"
 done
